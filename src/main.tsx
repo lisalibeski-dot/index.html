688fd278741e34b9cb0c1313df775b202262ef18
lisalibeski-dot <lisa.libeski@gmail.com>
2026-03-09 15:24:43 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx'; 
-import portfolio de Lisa Libeski from './App.tsx'; 
+import LisaLibeskiPortfolio from './App.tsx'; 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <LisaLibeskiPortfolio/>
   </StrictMode>,
 );
